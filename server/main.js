0fc634145f898dc434632f0519ef3d0d40a688ac
mad-ops <vish@mad-ops.com>
2018-11-25 20:02:29 -0500

    Remove start up init

--- server/main.js
+++ server/main.js
@@ -1,13 +1,3 @@
 import '../imports/api/inventory.js';
 import { Inventory } from '../imports/api/inventory.js';
 
-Meteor.startup(() => {
-	if(Inventory.find().count() === 0){ // if collecton is empty initialize to 0
-		var doc = {
-			tagIdSelector: "VM1000", tagFront2: "DOX", tagFront3: "250", tagBack1: "VISH", tagBack2: "14 KT YG", tagBack3: "0.50 G CTRN",
-			idnum: 1000, idtype: "VM"
-		}
-		Inventory.insert(doc)
-	}
-});
-
